387640f2fce7b5be18dec5b72dd733e21624b8cd
Umisyo <kusunokisouta@gmail.com>
2018-11-22 15:14:03 +0900

    1byteのデータを受け取って出力するだけ

--- Tamada/Tamada-ver1.0.py
+++ Tamada/Tamada-ver1.0.py
@@ -1,9 +1,8 @@
 import serial
+import time
 
-ser = serial.Serial('/dev/ttyACM0',9600,timeout = 0.1)
-String_data = ser.read()
-int_data = int.from_bytes(String_data , 'big')
-
-print(int_data)
-
-ser.close
+while True:
+    ser = serial.Serial('/dev/ttyACM0',9600,timeout = 0.1)
+    String_data = ser.read()
+    int_data = int.from_bytes(String_data , 'big')
+    print(int_data)
