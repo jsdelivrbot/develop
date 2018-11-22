8e80f2ba3ec16fbf1679959d93ba6c652ed821a4
Umisyo <kusunokisouta@gmail.com>
2018-11-22 17:40:22 +0900

    アルディーノからリスト型で送られてきたデータの中身を取り出せるようにした

--- Tamada/Tamada-ver1.0.py
+++ Tamada/Tamada-ver1.0.py
@@ -1,8 +1,15 @@
 import serial
 import time
 
-while True:
+def main():
     ser = serial.Serial('/dev/ttyACM0',9600,timeout = 0.1)
     String_data = ser.read()
     int_data = int.from_bytes(String_data , 'big')
+    int_data = int_data * 2
     print(int_data)
+
+if __name__ == "__main__":
+    while True:
+        main()
+
+ser.close
---
+++ Tamada/Tamada-ver2.0.py
@@ -0,0 +1,27 @@
+import serial
+import time
+
+val_size = 2
+values = [0 for x in range(val_size)]
+isValids = [False for x in range(val_size)]
+ser = serial.Serial('/dev/ttyACM0',9600,timeout = 0.1)
+
+while True:
+    headByte = ser.read()
+    head = int.from_bytes(headByte, 'big')
+    if head == 128:
+        isValids = [False for x in range(val_size)]
+
+    for i in range(val_size):
+        if head == 128+i:
+            highByte = ser.read()
+            lowByte = ser.read()
+            high = int.from_bytes(highByte, 'big')
+            low = int.from_bytes(lowByte, 'big')
+            values[i] = (high<<7) + low
+            if 0 <= values[i] and values[i] <= 1023:
+                isValids[i] = True
+
+    if all(i == True for i in isValids):
+        x = values[0]
+        print(x)
